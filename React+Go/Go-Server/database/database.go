d42b8cae166bbe5742c5a2ece10b33132debe352
kota-karthik <kotakarthik2307@gmail.com>
2023-12-20 19:37:54 +0530

    added database.go

---
+++ React+Go/Go-Server/database/database.go
@@ -0,0 +1,38 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ListSync/React+Go/Go-Server/utils"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+
+	get := utils.GetEnvWithKey
+	checkErr := utils.CheckErr
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(get("DATABASE")))
+	checkErr(err)
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	checkErr(err)
+	fmt.Println("Connected to MongoDB!!")
+	return client
+}
+
+// Client instance
+var DB *mongo.Client = ConnectDB()
+
+// getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	get := utils.GetEnvWithKey
+	collection := client.Database(get("DBNAME")).Collection(collectionName)
+	return collection
+}
--- React+Go/Go-Server/main.go
+++ React+Go/Go-Server/main.go
@@ -4,21 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"github.com/ListSync/React+Go/Go-Server/utils"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	checkErr:=utils.checkErr
+	checkErr := utils.CheckErr
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 
 		rw.Header().Set("Content-Type", "application/json")
-
+		fmt.Println("I love you 3000!!")
 		json.NewEncoder(rw).Encode(map[string]string{"data": "Hello from Mux & mongoDB"})
 	}).Methods("GET")
-	
-	checkErr(http.ListenAndServe(":6000", router))
 	fmt.Println("listening to port 6000!!")
+	checkErr(http.ListenAndServe(":6000", router))
+
 }
